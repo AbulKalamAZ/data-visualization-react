b172fcf87b107525debe71d9493d53d13af42697
azad <dev.akazad@hotmail.com>
2020-03-04 08:27:13 +0600

    Updates

---
+++ precache-manifest.8843d88c5b355eeeb2b291094ce9af04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26cc7b4d8009530e775296dd3ee650a5",
+    "url": "/data-visualization-react/index.html"
+  },
+  {
+    "revision": "26da09332e8c5224bd49",
+    "url": "/data-visualization-react/static/css/main.b93b11f4.chunk.css"
+  },
+  {
+    "revision": "bb0af3b57e1997eee2a8",
+    "url": "/data-visualization-react/static/js/2.4d9743a2.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/data-visualization-react/static/js/2.4d9743a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26da09332e8c5224bd49",
+    "url": "/data-visualization-react/static/js/main.d294e90e.chunk.js"
+  },
+  {
+    "revision": "80556d5994504e35c367",
+    "url": "/data-visualization-react/static/js/runtime-main.16d7d142.js"
+  }
+]);
